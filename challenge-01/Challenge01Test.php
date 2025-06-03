770d546c6a4a484fe7e34304c4dbb996f736c357
Eliecer David <eparialhuay@gmail.com>
2025-06-03 13:53:54 -0500

    add use cases in test from readme

--- challenge-01/Challenge01Test.php
+++ challenge-01/Challenge01Test.php
@@ -1,13 +1,23 @@
 <?php
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 require_once 'index.php';
 
 class Challenge01Test extends TestCase
 {
-    public function test()
+    static public function useCasesprovider(): array
     {
-        $this->assertEquals('1, 4, 13', findPoint(['1, 3, 4, 7, 13', '1, 2, 4, 13, 15']));
+        return [
+            [['1, 3, 4, 7, 13', '1, 2, 4, 13, 15'], '1, 4, 13'],
+            [['1, 3, 9, 10, 17, 18', '1, 4, 9, 10'], '1, 9, 10'],
+        ];
+    }
+
+    #[DataProvider('useCasesprovider')]
+    public function test($array, $result)
+    {
+        $this->assertEquals($result, findPoint($array));
     }
 }
